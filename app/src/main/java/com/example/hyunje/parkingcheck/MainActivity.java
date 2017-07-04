13c346b33da172f842a9a43486a4ea006066cc20
bnow0806 <guswp1993@naver.com>
2017-07-05 00:56:20 +0900

    7.5

--- app/src/main/java/com/example/hyunje/parkingcheck/MainActivity.java
+++ app/src/main/java/com/example/hyunje/parkingcheck/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.hyunje.parkingcheck;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity {
+GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gMap=((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.541,126.986),15));
     }
 }
