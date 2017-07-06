be26d1adb46b2f4cffae08f7c4bddae59a5a5af6
bnow0806 <guswp1993@naver.com>
2017-07-06 14:44:49 +0900

    7.6

--- app/src/main/java/com/example/hyunje/parkingcheck/MainActivity.java
+++ app/src/main/java/com/example/hyunje/parkingcheck/MainActivity.java
@@ -5,18 +5,31 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
 GoogleMap gMap;
+double lat=37.541;
+double lng=126.986;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gMap=((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.541,126.986),15));
+       SupportMapFragment gMap = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        gMap.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(new LatLng(lat, lng));
+        googleMap.addMarker(marker).showInfoWindow();
     }
 }
